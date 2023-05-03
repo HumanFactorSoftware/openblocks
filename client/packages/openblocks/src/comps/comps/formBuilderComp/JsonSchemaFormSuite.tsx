bfb502e2877eec1740a6a5f89266b98102b73397
chrissiemhrk <chrissiemhrk@gmail.com>
2023-05-03 12:28:04 +0200

    style(JsonSchemaFormSuite.tsx): add styled-components for form builder container and form inputs fix(JsonSchemaFormSuite.tsx): replace form-control class with ant-input class for form inputs

--- client/packages/openblocks/src/comps/comps/formBuilderComp/JsonSchemaFormSuite.tsx
+++ client/packages/openblocks/src/comps/comps/formBuilderComp/JsonSchemaFormSuite.tsx
@@ -4,6 +4,8 @@ import { withTheme } from "@rjsf/core";
 import { Theme } from "@rjsf/antd";
 import { Alert, Tabs } from "antd";
 import { TabContainerStyleType } from "comps/controls/styleControlConstants";
+import { ContainerStyleType } from "comps/controls/styleControlConstants";
+
 import styled, { css } from "styled-components";
 import {
   FormBuilder,
@@ -57,6 +59,61 @@ function checkError(text: string, language: string) {
   }
   return "";
 }
+
+const getWrapperStyle = (style: ContainerStyleType) => {
+  return css`
+    background-color: ${style.background};
+    border-color: ${style.border};
+    border-radius: ${style.radius};
+    border-width: ${style.borderWidth};
+    border-style: solid;
+    margin: ${style.margin};
+    padding: ${style.padding};
+
+    .form-builder {
+      [class^="formHead"] {
+        background-color: ${style.background};
+        border-color: ${style.border};
+        border-radius: ${style.radius};
+        border-width: ${style.borderWidth};
+        border-style: solid;
+        margin: ${style.margin};
+        padding: ${style.padding};
+        display: flex;
+      }
+      [class^="formHead"] div {
+        padding: ${style.padding};
+        width: 100%;
+        &:first-child {
+          margin-right: 0.6rem;
+        }
+      }
+    }
+
+    [class^="formBuilder"] .card-container {
+      background-color: ${style.background};
+      border-color: ${style.border};
+      border-radius: ${style.radius};
+      border-width: ${style.borderWidth};
+      border-style: solid;
+      margin: ${style.margin};
+      padding: ${style.padding};
+
+      &:hover {
+        border-color: ${style.border};
+      }
+    }
+    [class^="cardEntries"] .card-select {
+      border: inherit;
+      border-radius: inherit;
+    }
+
+    [class^="cardEntries"] input {
+      border: 1px solid #d7d9e0;
+      border-radius: ${style.radius};
+    }
+  `;
+};
 
 const getStyle = (style: TabContainerStyleType) => {
   return css`
@@ -135,6 +192,12 @@ const StyledTabs = styled(Tabs)<{
   ${(props) => props.$style && getStyle(props.$style)}
 `;
 
+const Wrapper = styled.div<{ $style: ContainerStyleType }>`
+  display: flex;
+  flex-flow: column;
+  ${(props) => props.$style && getWrapperStyle(props.$style)}
+`;
+
 class JsonSchemaFormEditor extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
@@ -151,6 +214,12 @@ class JsonSchemaFormEditor extends React.Component<Props, State> {
     };
   }
 
+  componentDidMount() {
+    const myElements = document.querySelectorAll(".form-control");
+    myElements.forEach((element) => {
+      element.classList.replace("form-control", "ant-input");
+    });
+  }
   // update state schema and indicate parsing errors
   updateSchema(text: string) {
     // update parent
@@ -186,25 +255,24 @@ class JsonSchemaFormEditor extends React.Component<Props, State> {
         label: "Visual Form Builder",
         key: "form-builder",
         children: (
-          <div
+          <Wrapper
             className="tab-pane"
-            style={{
-              height: this.props.height ? this.props.height : "auto",
-            }}
+            $style={this.props.style}
+            style={{ height: this.props.height ? this.props.height : "900px" }}
           >
             <ErrorBoundary onErr={() => {}}>
               <FormBuilder
                 schema={this.props.schema}
                 uischema={this.props.uischema}
                 mods={this.props.mods}
-                classes={""}
+                className="form-builder"
                 onChange={(newSchema: string, newUiSchema: string) => {
                   if (this.props.onChange)
                     this.props.onChange(newSchema, newUiSchema);
                 }}
               />
             </ErrorBoundary>
-          </div>
+          </Wrapper>
         ),
       },
       {
